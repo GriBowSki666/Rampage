198cac1afe03bc5d667f61bec00bb4ef758e625c
Алексей Хрулев <sabaevsrib@gmail.com>
2025-06-28 12:21:06 +0300

    first commit

--- script.js
+++ script.js
@@ -1,19 +1,6 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const form = document.getElementById('appointmentForm');
-    const appointmentDateInput = document.getElementById('appointmentDate');
-
-    const today = new Date();
-    const tomorrow = new Date(today)
-    tomorrow.setDate(tomorrow.getDate() + 1);
-    const minDate = tomorrow.toISOString().split('T')[0];
-    appointmentDateInput.min = minDate;
-
-    form.addEventListener('submit', function (e) {
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementByld('appointmentForm').addEventListener('submit', e => {
         e.preventDefault();
-
-        if (validateForm()) {
-            alert('Ваша заявка успешно отправленна')
-            form.reset();
-        }
+        alert('Ваша заявка успешно отправленна');
     });
-})
+});
